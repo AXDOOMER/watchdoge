0d57c00b6a6ee9798b58506da968275e3f03cb77
Alexandre-Xavier Labonté-Lamoureux <alexandrexavier@live.ca>
2017-06-21 21:22:58 -0400

    Code that works

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # watchdoge
 Port scanner for vulnerabilities
+
+### Usage
+
+```
+> go build watchdog.go 
+> ./watchdog 192.168.1.1
+tcp port 21 open
+tcp port 22 open
+tcp port 53 open
+tcp port 80 open
+tcp port 2000 open
+tcp port 8291 open
+tcp port 8728 open
+tcp port 8729 open
+done
+```
+
+##### Wow security. Much port scan. 
+![doggo](http://i3.kym-cdn.com/entries/icons/original/000/021/033/Screenshot_236.png)
+
+
--- watchdog.go
+++ watchdog.go
@@ -2,35 +2,38 @@ package main
 
 import (
 	"os"
+	"strconv"
 	"fmt"
 	"net"
 	"time"
 )
 
-// nmap -p 36367 localhost
-// nmap -p {00000..65535} localhost
-
 func test(protocol, host string, port int) bool {
-				// net.ListenTCP ?						// 100 * time.Millisecond
-	conn, err := net.DialTimeout(protocol, host + ":" + port, 1 * time.Second)
+	conn, err := net.DialTimeout(protocol, host + ":" + strconv.Itoa(port), 1 * time.Second)
 	if err != nil {
-		return false;	// Cannot connect
+		return false	// Cannot connect
 	}
 	/*defer*/ conn.Close()
-	return true;	// Connection successful
+	return true	// Connection successful
 }
 
 func listen(protocol, host string, port int) bool {
-				// net.ListenTCP ?
-	ln, err := net.Listen(protocol, host + ":" + port)
+	ln, err := net.Listen(protocol, host + ":" + strconv.Itoa(port))
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Can't listen on port %q: %s", port, err)
+		//fmt.Fprintf(os.Stderr, "Can't listen on port %q: %s", port, err)
+		return false
 	}
 	ln.Close()
+	return true
 }
 
 func main() {
 	
+	if len(os.Args) < 2 {
+		fmt.Printf("No IP address specified, please pass one as a paremeter on the CLI.\n")
+		os.Exit(1)
+	}
+
 	ip := os.Args[1]	// Get the IP from the command line
 	
 	for port := 0; port <= 65535; port++ {
@@ -41,15 +44,14 @@ func main() {
 			fmt.Printf("tcp port %d open\n", port)
 		}
 		
-		//Connect udp
-		if test("udp", ip, port) == true {
+		//Connect UDP
+		/*if test("udp", ip, port) == true {
 			
-			fmt.Println("udp port " + port + " open")
-		}
-	
-		fmt.Println("done")
-
+			fmt.Println("udp port " + strconv.Itoa(port) + " open")
+		}*/
 	}
+
+	fmt.Println("done")
 }
 
 
